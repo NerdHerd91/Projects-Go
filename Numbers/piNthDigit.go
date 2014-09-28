111e20019d3fe0244812e4b7e14459394c3e8db2
NerdHerd91 <sventrel@outlook.com>
2014-09-28 14:32:33 -0500

    Add function to compute the nth decimal place of pi

---
+++ Numbers/piNthDigit.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "math"
+    "os"
+    "strconv"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        log.Fatal("An integer N must be provided as the first argument.")
+    }
+
+    // Attempt to parse the argument for n.
+    n, err := strconv.Atoi(os.Args[1])
+    if err != nil {
+        log.Fatal(err)
+    }
+    pi := computePi()
+
+    // Format the value of pi to the number of digits requested.
+    fmt.Printf("The value of Pi to %v digits: %v\n", n, strconv.FormatFloat(pi, 'f', n, 64)) 
+}
+
+func computePi() (float64) {
+    // Use Machin's Formula to compute Pi.
+    x := float64(1) / 5
+    y := float64(1) / 239
+    return 16 * math.Atan(x) - 4 * math.Atan(y) 
+}
